9e60a99d5db7f80572e4e093504059089e3052f1
nguyenq <nguyenq@users.sf.net>
2015-06-10 16:28:06 -0500

    Add support for image conversion

---
+++ src/main/java/net/sourceforge/lept4j/util/LeptUtils.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2015 Quan Nguyen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sourceforge.lept4j.util;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Iterator;
+import java.util.Locale;
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.metadata.IIOMetadata;
+import javax.imageio.stream.ImageOutputStream;
+
+import com.sun.media.imageio.plugins.tiff.TIFFImageWriteParam;
+import com.sun.jna.ptr.PointerByReference;
+
+import com.ochafik.lang.jnaerator.runtime.NativeSize;
+import com.ochafik.lang.jnaerator.runtime.NativeSizeByReference;
+
+import static net.sourceforge.lept4j.ILeptonica.IFF_TIFF;
+import net.sourceforge.lept4j.Leptonica1;
+import net.sourceforge.lept4j.Pix;
+
+/**
+ * Various utility methods for Leptonica.
+ *
+ */
+public class LeptUtils {
+
+    final static String JAI_IMAGE_WRITER_MESSAGE = "Need to install JAI Image I/O package.\nhttps://java.net/projects/jai-imageio/";
+    final static String TIFF_FORMAT = "tiff";
+
+    /**
+     * Converts Leptonica <code>Pix</code> to <code>BufferedImage</code>.
+     *
+     * @param pix
+     * @return BufferedImage
+     * @throws IOException
+     */
+    public static BufferedImage convertPixToImage(Pix pix) throws IOException {
+        PointerByReference pdata = new PointerByReference();
+        NativeSizeByReference psize = new NativeSizeByReference();
+        int format = IFF_TIFF;
+        Leptonica1.pixWriteMem(pdata, psize, pix, format);
+        byte[] b = pdata.getValue().getByteArray(0, psize.getValue().intValue());
+        InputStream in = new ByteArrayInputStream(b);
+        BufferedImage bi = ImageIO.read(in);
+        in.close();
+        return bi;
+    }
+
+    /**
+     * Converts <code>BufferedImage</code> to Leptonica <code>Pix</code> .
+     *
+     * @param image
+     * @return Pix
+     * @throws IOException
+     */
+    public static Pix convertImageToPix(BufferedImage image) throws IOException {
+        ByteBuffer buff = getImageByteBuffer(image);
+        Pix pix = Leptonica1.pixReadMem(buff, new NativeSize(buff.capacity()));
+        return pix;
+    }
+
+    /**
+     * Gets image data of an <code>RenderedImage</code> object.
+     *
+     * @param image an <code>RenderedImage</code> object
+     * @return a byte buffer of image data
+     * @throws IOException
+     */
+    static ByteBuffer getImageByteBuffer(RenderedImage image) throws IOException {
+        //Set up the writeParam
+        TIFFImageWriteParam tiffWriteParam = new TIFFImageWriteParam(Locale.US);
+        tiffWriteParam.setCompressionMode(ImageWriteParam.MODE_DISABLED);
+
+        //Get tif writer and set output to file
+        Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName(TIFF_FORMAT);
+
+        if (!writers.hasNext()) {
+            throw new RuntimeException(JAI_IMAGE_WRITER_MESSAGE);
+        }
+
+        ImageWriter writer = writers.next();
+
+        //Get the stream metadata
+        IIOMetadata streamMetadata = writer.getDefaultStreamMetadata(tiffWriteParam);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ImageOutputStream ios = ImageIO.createImageOutputStream(outputStream);
+        writer.setOutput(ios);
+        writer.write(streamMetadata, new IIOImage(image, null, null), tiffWriteParam);
+//        writer.write(image);
+        writer.dispose();
+        ios.seek(0);
+        byte[] b = new byte[(int) ios.length()];
+        ios.read(b);
+        ios.close();
+
+        ByteBuffer buf = ByteBuffer.allocateDirect(b.length);
+        buf.order(ByteOrder.nativeOrder());
+        buf.put(b);
+        buf.flip();
+        return buf;
+    }
+}
---
+++ src/test/java/net/sourceforge/lept4j/util/LeptUtilsTest.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2015 Quan Nguyen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sourceforge.lept4j.util;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import javax.imageio.ImageIO;
+
+import net.sourceforge.lept4j.Leptonica;
+import net.sourceforge.lept4j.Pix;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class LeptUtilsTest {
+
+    private final String testResourcesPath = "src/test/resources/test-data";
+
+    public LeptUtilsTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of convertPixToImage method, of class LeptUtils.
+     *
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testConvertPixToImage() throws Exception {
+        System.out.println("convertPixToImage");
+        File input = new File(testResourcesPath, "eurotext.png");
+        Leptonica leptInstance = Leptonica.INSTANCE;
+        Pix pix = leptInstance.pixRead(input.getPath());
+        BufferedImage result = LeptUtils.convertPixToImage(pix);
+        assertEquals(pix.w, result.getWidth());
+        assertEquals(pix.h, result.getHeight());
+        assertEquals(pix.d, result.getColorModel().getPixelSize());
+        System.out.println(String.format("Image properties: width=%d, height=%d, depth=%d", result.getWidth(), result.getHeight(), result.getColorModel().getPixelSize()));
+    }
+
+    /**
+     * Test of convertImageToPix method, of class LeptUtils.
+     *
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testConvertImageToPix() throws Exception {
+        System.out.println("convertImageToPix");
+        File input = new File(testResourcesPath, "eurotext.png");
+        BufferedImage image = ImageIO.read(new FileInputStream(input));
+        Pix result = LeptUtils.convertImageToPix(image);
+        assertEquals(image.getWidth(), result.w);
+        assertEquals(image.getHeight(), result.h);
+        assertEquals(image.getColorModel().getPixelSize(), result.d);
+        System.out.println(String.format("Image properties: width=%d, height=%d, depth=%d", result.w, result.h, result.d));
+    }
+
+}
